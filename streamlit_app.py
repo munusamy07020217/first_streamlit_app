04e40df7e86497b5251fa9f8b914ff7868c4c840
munusamy07020217 <118072827+munusamy07020217@users.noreply.github.com>
2022-11-13 00:07:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 
 import streamlit
 streamlit.title('My paraents new healthy diner')
-streamlit.header(🥣'Breakfast Menu')
-streamlit.text(🥗'Omega 3 & Blueberry Oatmeal')
-streamlit.text(🐔'Kale, Spinach & Rocket Smoothie')
-streamlit.text(🥑🍞 'Hard-Boiled Free-Range Egg')
+streamlit.header('🥣 Breakfast Menu')
+streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🍞 Hard-Boiled Free-Range Egg')
    
