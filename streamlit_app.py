05a3327f2143ddeb21c71f3371194cc7dc43b0b1
munusamy07020217 <118072827+munusamy07020217@users.noreply.github.com>
2022-11-13 22:03:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,20 @@
 
 import streamlit
+import pandas
+
 streamlit.header('🥣 My mom''s new healthy dinner')
 streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🍞 Hard-Boiled Free-Range Egg')
 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇') 
 
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-   
-import pandas
 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 my_fruit_list = my_fruit_list.set_index('Fruit')
+
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
